3fbe731937799dfb0b8314f3149a49803353a9ee
avneetsingh99 <96041683+avneetsingh99@users.noreply.github.com>
2021-12-15 12:53:03 +0530

    Add files via upload

---
+++ finding prime or not prime number.py
@@ -0,0 +1,16 @@
+num=int(input("enter a number"))
+k=0
+if num==0 or num==1:
+ print("not a prime number")
+else:
+    i=2
+    while(i<num):
+        if num%i==0:
+           k=k+1
+        i=i+1
+
+if k==0:
+ print("prime")
+else:
+ print("not prime")
+
